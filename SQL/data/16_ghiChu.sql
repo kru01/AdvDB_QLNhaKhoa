0fde1c4100f7a24db9d0523458ff68caa79a97d3
Phong <phongan105@gmail.com>
2023-11-29 21:31:43 +0700

    Add ghiChu

---
+++ SQL/data/16_ghiChu.sql
@@ -0,0 +1,33 @@
+USE [NC03_QLNhaKhoa]
+GO
+
+--DM: 1
+EXEC USP_GHICHU_INS
+ 'HS00001', 'Dozidine MR 35mg , Azopt', 1
+--DM: 2
+EXEC USP_GHICHU_INS
+ 'HS00002', 'Phát ban', 0
+--DM: 3
+EXEC USP_GHICHU_INS
+ 'HS00003', 'Bricanyl, Coversyl 5mg, Debridat', 1
+--DM: 4
+EXEC USP_GHICHU_INS
+ 'HS00004', 'Nổi mề đay', 0
+--DM: 5
+EXEC USP_GHICHU_INS
+ 'HS00005', 'Sưng lưỡi', 0
+--DM: 6
+EXEC USP_GHICHU_INS
+ 'HS00006', 'Sưng lưỡi', 0
+--DM: 7
+EXEC USP_GHICHU_INS
+ 'HS00007', 'Nổi mề đay', 0
+--DM: 8
+EXEC USP_GHICHU_INS
+ 'HS00008', 'Ngứa da', 0
+--DM: 9
+EXEC USP_GHICHU_INS
+ 'HS00009', 'Phát ban', 0
+--DM: 10
+EXEC USP_GHICHU_INS
+ 'HS00010', 'Hắt xì - nghẹt mũi', 0

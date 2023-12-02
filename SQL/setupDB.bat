c0d056ca2d6b920d9248f912e5b762b5ab92ae0e
Phong <phongan105@gmail.com>
2023-12-02 23:26:17 +0700

    Update LICHHEN data and change setupDB

--- SQL/setupDB.bat
+++ SQL/setupDB.bat
@@ -18,8 +18,53 @@ for %%G in (.\storedProcs\*.sql) do SQLCMD -E -dmaster -f65001 -i"%%G"
 @echo ---- Creating Indices...
 SQLCMD -E -dmaster -f65001 -i".\indices.sql"
 
-@echo ---- Populating Data...
-for %%G in (.\data\*.sql) do SQLCMD -E -dmaster -f65001 -i"%%G"
+@echo ---- Populating taiKhoan.sql
+SQLCMD -E -dmaster -f65001 -i"\data\1_taiKhoan.sql"
+
+@echo ---- Populating beMatRang.sql
+SQLCMD -E -dmaster -f65001 -i"\data\2_beMatRang.sql"
+
+@echo ---- Populating danhMuc.sql
+SQLCMD -E -dmaster -f65001 -i"\data\3_danhMuc.sql"
+
+@echo ---- Populating lieuTrinh.sql
+SQLCMD -E -dmaster -f65001 -i"\data\4_lieuTrinh.sql"
+
+@echo ---- Populating hoSoBenhNhan.sql
+SQLCMD -E -dmaster -f65001 -i"\data\5_hoSoBenhNhan.sql"
+
+@echo ---- Populating phongKham.sql
+SQLCMD -E -dmaster -f65001 -i"\data\6_phongKham.sql"
+
+@echo ---- Populating rang.sql
+SQLCMD -E -dmaster -f65001 -i"\data\7_rang.sql"
+
+@echo ---- Populating thuoc.sql
+SQLCMD -E -dmaster -f65001 -i"\data\8_thuoc.sql"
+
+@echo ---- Populating keHoachDieuTri.sql
+SQLCMD -E -dmaster -f65001 -i"\data\9_keHoachDieuTri.sql"
+
+@echo ---- Populating lichHen.sql
+SQLCMD -E -dmaster -f65001 -i"\data\10_lichHen.sql"
+
+@echo ---- Populating lichNgay.sql
+SQLCMD -E -dmaster -f65001 -i"\data\11_lichNgay.sql"
+
+@echo ---- Populating lichThang.sql
+SQLCMD -E -dmaster -f65001 -i"\data\12_lichThang.sql"
+
+@echo ---- Populating lichTuan.sql
+SQLCMD -E -dmaster -f65001 -i"\data\13_lichTuan.sql"
+
+@echo ---- Populating toaThuoc.sql
+SQLCMD -E -dmaster -f65001 -i"\data\14_toaThuoc.sql"
+
+@echo ---- Populating thanhToan.sql
+SQLCMD -E -dmaster -f65001 -i"\data\15_thanhToan.sql"
+
+@echo ---- Populating ghiChu.sql
+SQLCMD -E -dmaster -f65001 -i"\data\16_ghiChu.sql"
 
 @echo ---- Granting Permissions...
 SQLCMD -E -dmaster -f65001 -i".\permissions.sql"

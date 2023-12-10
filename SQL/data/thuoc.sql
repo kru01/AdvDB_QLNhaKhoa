2181c04e20d2398c55658a83cc2dc6c4f8a4e6d3
Phong <phongan105@gmail.com>
2023-12-10 08:09:49 +0700

    Update some data

--- SQL/data/thuoc.sql
+++ SQL/data/thuoc.sql
@@ -3,91 +3,91 @@ GO
 
 --THUOC: 1
 EXEC USP_THUOC_INS
- N'Broncho-Vaxom Adults', 2, N'Không dành cho trẻ em dưới 10 tuổi', 74, '2025/02/08', 9000, 'AD008'
+ N'Broncho-Vaxom Adults', 2, N'Không dành cho trẻ em dưới 10 tuổi', 74, '2025/02/08', 9000
 --THUOC: 2
 EXEC USP_THUOC_INS
- N'4mg/5mg "', 0, N'Uống trước khi ăn', 26, '2025/01/07', 7000, 'AD003'
+ N'4mg/5mg "', 0, N'Uống trước khi ăn', 26, '2025/01/07', 7000
 --THUOC: 3
 EXEC USP_THUOC_INS
- N'Enterogermina 2 tỷ bào tử/ 5ml  ', 1, N'Uống sau khi ăn', 14, '2025/05/04', 15000, 'AD008'
+ N'Enterogermina 2 tỷ bào tử/ 5ml  ', 1, N'Uống sau khi ăn', 14, '2025/05/04', 15000
 --THUOC: 4
 EXEC USP_THUOC_INS
- N'Esomeprazol Stada 40mg', 0, N'Uống sau khi ăn', 14, '2025/12/31', 18000, 'AD003'
+ N'Esomeprazol Stada 40mg', 0, N'Uống sau khi ăn', 14, '2025/12/31', 18000
 --THUOC: 5
 EXEC USP_THUOC_INS
- N'Ciprobay 200mg/100ml', 2, N'Không dành cho trẻ em dưới 10 tuổi', 92, '2024/09/02', 1000, 'AD010'
+ N'Ciprobay 200mg/100ml', 2, N'Không dành cho trẻ em dưới 10 tuổi', 92, '2024/09/02', 1000
 --THUOC: 6
 EXEC USP_THUOC_INS
- N'Cồn 70°', 0, N'Uống trước khi ăn', 95, '2024/05/31', 13000, 'AD005'
+ N'Cồn 70°', 0, N'Uống trước khi ăn', 95, '2024/05/31', 13000
 --THUOC: 7
 EXEC USP_THUOC_INS
- N'Exforge', 1, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 82, '2024/04/29', 15000, 'AD005'
+ N'Exforge', 1, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 82, '2024/04/29', 15000
 --THUOC: 8
 EXEC USP_THUOC_INS
- N'Ciprobay 500mg', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 13, '2025/07/31', 8000, 'AD004'
+ N'Ciprobay 500mg', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 13, '2025/07/31', 8000
 --THUOC: 9
 EXEC USP_THUOC_INS
- N'Diovan 80', 2, N'Uống sau khi ăn', 33, '2025/07/08', 3000, 'AD009'
+ N'Diovan 80', 2, N'Uống sau khi ăn', 33, '2025/07/08', 3000
 --THUOC: 10
 EXEC USP_THUOC_INS
- N'Betaserc 16mg', 1, N'Uống sau khi ăn', 60, '2024/10/05', 7000, 'AD001'
+ N'Betaserc 16mg', 1, N'Uống sau khi ăn', 60, '2024/10/05', 7000
 --THUOC: 11
 EXEC USP_THUOC_INS
- N'Betadine Antiseptic Solution 10%w/v', 0, N'Không dành cho trẻ em dưới 10 tuổi', 16, '2024/12/04', 17000, 'AD001'
+ N'Betadine Antiseptic Solution 10%w/v', 0, N'Không dành cho trẻ em dưới 10 tuổi', 16, '2024/12/04', 17000
 --THUOC: 12
 EXEC USP_THUOC_INS
- N'Bricanyl', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 22, '2024/09/07', 16000, 'AD008'
+ N'Bricanyl', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 22, '2024/09/07', 16000
 --THUOC: 13
 EXEC USP_THUOC_INS
- N'Buscopan 20mg/ml', 0, N'Uống sau khi ăn', 90, '2024/10/07', 19000, 'AD004'
+ N'Buscopan 20mg/ml', 0, N'Uống sau khi ăn', 90, '2024/10/07', 19000
 --THUOC: 14
 EXEC USP_THUOC_INS
- N'Cammic', 2, N'Không dành cho trẻ em dưới 10 tuổi', 83, '2024/08/07', 15000, 'AD004'
+ N'Cammic', 2, N'Không dành cho trẻ em dưới 10 tuổi', 83, '2024/08/07', 15000
 --THUOC: 15
 EXEC USP_THUOC_INS
- N'Adalat LA 30mg', 1, N'Không dành cho phụ nữ có thai', 28, '2025/07/22', 5000, 'AD006'
+ N'Adalat LA 30mg', 1, N'Không dành cho phụ nữ có thai', 28, '2025/07/22', 5000
 --THUOC: 16
 EXEC USP_THUOC_INS
- N'Cozaar 50mg', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 46, '2024/05/24', 1000, 'AD001'
+ N'Cozaar 50mg', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 46, '2024/05/24', 1000
 --THUOC: 17
 EXEC USP_THUOC_INS
- N'Bupivacaine WPW Spinal Heavy 0.5%4ml', 1, N'Uống sau khi ăn', 15, '2024/09/25', 18000, 'AD009'
+ N'Bupivacaine WPW Spinal Heavy 0.5%4ml', 1, N'Uống sau khi ăn', 15, '2024/09/25', 18000
 --THUOC: 18
 EXEC USP_THUOC_INS
- N'Carduran', 0, N'Uống sau khi ăn', 67, '2024/01/23', 7000, 'AD007'
+ N'Carduran', 0, N'Uống sau khi ăn', 67, '2024/01/23', 7000
 --THUOC: 19
 EXEC USP_THUOC_INS
- N'Amoxicilin 500mg', 1, N'Uống sau khi ăn', 53, '2025/12/12', 12000, 'AD003'
+ N'Amoxicilin 500mg', 1, N'Uống sau khi ăn', 53, '2025/12/12', 12000
 --THUOC: 20
 EXEC USP_THUOC_INS
- N'Epokine Prefilled Injection 2000Units/0,5ml', 2, N'Không dành cho phụ nữ có thai', 35, '2025/11/05', 12000, 'AD003'
+ N'Epokine Prefilled Injection 2000Units/0,5ml', 2, N'Không dành cho phụ nữ có thai', 35, '2025/11/05', 12000
 --THUOC: 21
 EXEC USP_THUOC_INS
- N'Dorithricin', 1, N'Uống sau khi ăn', 45, '2024/06/21', 8000, 'AD001'
+ N'Dorithricin', 1, N'Uống sau khi ăn', 45, '2024/06/21', 8000
 --THUOC: 22
 EXEC USP_THUOC_INS
- N'Coversyl Plus Arginine', 0, N'Không dành cho trẻ em dưới 10 tuổi', 62, '2024/09/10', 18000, 'AD009'
+ N'Coversyl Plus Arginine', 0, N'Không dành cho trẻ em dưới 10 tuổi', 62, '2024/09/10', 18000
 --THUOC: 23
 EXEC USP_THUOC_INS
- N'Actrapid ', 0, N'Uống trước khi ăn', 17, '2025/09/28', 3000, 'AD006'
+ N'Actrapid ', 0, N'Uống trước khi ăn', 17, '2025/09/28', 3000
 --THUOC: 24
 EXEC USP_THUOC_INS
- N'Cerecaps', 2, N'Không dành cho trẻ em dưới 10 tuổi', 66, '2025/05/02', 4000, 'AD002'
+ N'Cerecaps', 2, N'Không dành cho trẻ em dưới 10 tuổi', 66, '2025/05/02', 4000
 --THUOC: 25
 EXEC USP_THUOC_INS
- N'Esomeprazol Stada 40mg', 2, N'Không dành cho trẻ em dưới 10 tuổi', 89, '2024/08/09', 2000, 'AD004'
+ N'Esomeprazol Stada 40mg', 2, N'Không dành cho trẻ em dưới 10 tuổi', 89, '2024/08/09', 2000
 --THUOC: 26
 EXEC USP_THUOC_INS
- N'Acyclovir Stada 800mg', 2, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 88, '2024/05/03', 5000, 'AD002'
+ N'Acyclovir Stada 800mg', 2, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 88, '2024/05/03', 5000
 --THUOC: 27
 EXEC USP_THUOC_INS
- N'Buscopan 20mg/ml', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 43, '2025/09/08', 18000, 'AD003'
+ N'Buscopan 20mg/ml', 0, N'Không dành cho người dị ứng với bất cứ thành phần nào của thuốc', 43, '2025/09/08', 18000
 --THUOC: 28
 EXEC USP_THUOC_INS
- N'Efferalgan 80mg', 2, N'Uống sau khi ăn', 47, '2024/07/04', 14000, 'AD002'
+ N'Efferalgan 80mg', 2, N'Uống sau khi ăn', 47, '2024/07/04', 14000
 --THUOC: 29
 EXEC USP_THUOC_INS
- N'Daflon', 0, N'Không dành cho trẻ em dưới 10 tuổi', 52, '2025/08/13', 11000, 'AD007'
+ N'Daflon', 0, N'Không dành cho trẻ em dưới 10 tuổi', 52, '2025/08/13', 11000
 --THUOC: 30
 EXEC USP_THUOC_INS
- N'Auclanityl 875mg/125mg', 1, N'Uống trước khi ăn', 58, '2025/11/27', 14000, 'AD006'
+ N'Auclanityl 875mg/125mg', 1, N'Uống trước khi ăn', 58, '2025/11/27', 14000
